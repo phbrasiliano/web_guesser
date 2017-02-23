7a079444d2281861a035edb044672677647eccff
Phellipe <phbrasiliano@gmail.com>
2017-02-23 13:29:21 +0100

    makes the number stay the same until the server is restarted

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+secret_num = rand(101)
 
 get '/' do
-  secret_num = rand(101)
-  "Your secret number is #{secret_num}!"
+  "Your secret number is #{secret_num}! :)"
 end
